16a95b2a23f0e2945373c238b9b6f952b9b42e19
quri <krysanv-k@mail.ru>
2025-08-19 09:15:10 +0300

    новый сервис

---
+++ src/main/java/com/quri/blog/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.quri.blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UsersRepo usersRepo;
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Users user = usersRepo.findByLogin(login);
+        if (user != null){
+            return new UserDetailsImpl(user);
+        }else throw new UsernameNotFoundException("User not found");
+
+    }
+}
--- src/main/java/com/quri/blog/UsersRepo.java
+++ src/main/java/com/quri/blog/UsersRepo.java
@@ -1,7 +1,8 @@
 package com.quri.blog;
 
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepo extends JpaRepository<Users, Long> {
-
+    Users findByLogin(String login);
 }
