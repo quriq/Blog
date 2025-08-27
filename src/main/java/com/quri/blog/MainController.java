aece5fb52efecfef251caf296e166141a6daad40
quri <krysanv-k@mail.ru>
2025-08-27 22:55:24 +0300

    добавлено начало

--- src/main/java/com/quri/blog/MainController.java
+++ src/main/java/com/quri/blog/MainController.java
@@ -1,14 +1,20 @@
 package com.quri.blog;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class MainController {
-
+@Autowired
+private PostsRepo postsRepo;
     @GetMapping("/")
-    public String home() {
+    public String home(Model model)
+    {
+        Iterable<Posts> posts = postsRepo.findAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
     @GetMapping("/about")
