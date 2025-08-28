8d30f9f9a968a51a8788a7c2c1d8eb7f14b6dbba
quri <krysanv-k@mail.ru>
2025-08-28 18:30:53 +0300

    Добавлены id

--- src/main/java/com/quri/blog/MainController.java
+++ src/main/java/com/quri/blog/MainController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Controller
@@ -37,5 +41,18 @@ private PostsRepo postsRepo;
         Posts posts = new Posts(title,anons,text);
         postsRepo.save(posts);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String id(@PathVariable(value = "id") long id, Model model){
+        if(!postsRepo.existsById(id)){
+            return "/blog";
+        }
+        Optional<Posts> posts = postsRepo.findById(id);
+        ArrayList<Posts> res = new ArrayList<>();
+        posts.ifPresent(res::add);
+        model.addAttribute("posts", res);
+        return "id";
+
     }
 }
